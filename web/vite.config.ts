4b4572e91e2768ea21eb5e7582e124fb0fe6734c
tomolld <tomoki2757@gmail.com>
2024-07-29 20:45:58 +0900

    searchroot

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { flatRoutes } from "remix-flat-routes";
 
 export default defineConfig({
 	plugins: [
@@ -9,6 +10,10 @@ export default defineConfig({
 				v3_fetcherPersist: true,
 				v3_relativeSplatPath: true,
 				v3_throwAbortReason: true,
+			},
+			ignoredRouteFiles: ["**/*"],
+			routes: async (defineRoutes) => {
+				return flatRoutes("routes", defineRoutes);
 			},
 		}),
 		tsconfigPaths(),
