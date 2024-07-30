4e417b7a38ab99953d505966f3b2c7fb1fd8f64a
tomolld <tomoki2757@gmail.com>
2024-07-30 23:36:21 +0900

    feat: Update header logo to use theme-specific image

--- web/app/components/Header.tsx
+++ web/app/components/Header.tsx
@@ -12,17 +12,19 @@ import {
 } from "~/components/ui/dropdown-menu";
 import { Separator } from "~/components/ui/separator";
 import type { SafeUser } from "~/types";
-
+import { useTheme } from "next-themes";
 interface HeaderProps {
 	safeUser: SafeUser | null;
 }
 
 export function Header({ safeUser }: HeaderProps) {
+	const { theme } = useTheme();
+
 	return (
 		<header className="shadow-sm mb-10 z-10 ">
 			<div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
 				<Link to="/">
-					<h1 className="text-3xl font-bold ">EveEve</h1>
+					<img src={`/title-logo-${theme}.png`} alt="" className="w-40" />
 				</Link>
 				<div className="flex items-center space-x-4">
 					<Button variant="ghost" size="icon">
