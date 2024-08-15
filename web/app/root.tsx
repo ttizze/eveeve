eacd5fff4f781d02529183f27cffebf0634ad85e
tomolld <tomoki2757@gmail.com>
2024-08-15 17:34:39 +0900

    f

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -29,7 +29,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 		<html lang="ja">
 			<head>
 				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1"
+				/>
 				<Meta />
 				<Links />
 			</head>
