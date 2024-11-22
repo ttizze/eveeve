8b179d612a3f888e0f5a12be62adee39858b5205
tomolld <tomoki2757@gmail.com>
2024-11-22 18:33:56 +0900

    ユーザーページにLikeボタンを追加し、ユーザーのいいね情報を取得する機能を実装。ページのレイアウトを調整。

--- web/app/routes/$userName+/page+/$slug+/components/ShareDialog.tsx
+++ web/app/routes/$userName+/page+/$slug+/components/ShareDialog.tsx
@@ -18,6 +18,7 @@ import {
 	DialogTrigger,
 } from "~/components/ui/dialog";
 
+
 interface ShareDialogProps {
 	url: string;
 	title: string;
@@ -27,7 +28,7 @@ export function ShareDialog({ url, title }: ShareDialogProps) {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<Dialog open={isOpen} onOpenChange={setIsOpen} modal={false}>
+		<Dialog open={isOpen} onOpenChange={setIsOpen} >
 			<DialogTrigger asChild>
 				<Button
 					variant="ghost"
@@ -42,12 +43,18 @@ export function ShareDialog({ url, title }: ShareDialogProps) {
 					<DialogTitle>Share</DialogTitle>
 				</DialogHeader>
 				<div className="flex justify-center space-x-4 mt-4">
-					<CopyIcon
-						className="w-4 h-4 ml-2 cursor-pointer"
-						onClick={() => {
-							navigator.clipboard.writeText(url);
-						}}
-					/>
+					<Button
+						variant="outline"
+						size="icon"
+						className="rounded-full"
+					>
+						<CopyIcon
+							className="w-4 h-4"
+							onClick={() => {
+								navigator.clipboard.writeText(url);
+							}}
+						/>
+					</Button>
 					<FacebookShareButton url={url}>
 						<FacebookIcon size={32} round />
 					</FacebookShareButton>
