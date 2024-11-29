de11bc1f609802ca467dcbbdf37f160e23362c4d
tomolld <tomoki2757@gmail.com>
2024-11-29 18:56:45 +0900

    f

--- web/app/routes/$userName+/page+/$slug+/components/sourceTextAndTranslationSection/SourceTextAndTranslationSection.tsx
+++ web/app/routes/$userName+/page+/$slug+/components/sourceTextAndTranslationSection/SourceTextAndTranslationSection.tsx
@@ -26,11 +26,10 @@ export function SourceTextAndTranslationSection({
 		<>
 			{showOriginal && (
 				<span
-					className={`inline-block ${
-						sourceTextWithTranslations.translationsWithVotes.length === 0
+					className={`inline-block ${sourceTextWithTranslations.translationsWithVotes.length === 0 || !showTranslation
 							? "text-gray-700 dark:text-gray-200"
 							: "text-gray-300 dark:text-gray-600"
-					} ${sourceTextClassName}`}
+						} ${sourceTextClassName}`}
 				>
 					{isPublished === false && <Lock className="h-6 w-6 mr-1 inline" />}
 					{elements}
