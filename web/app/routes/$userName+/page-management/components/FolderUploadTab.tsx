fb992590bf09ef684b442b0466b3a5a112f12afb
tomolld <tomoki2757@gmail.com>
2024-12-05 16:56:03 +0900

    f

--- web/app/routes/$userName+/page-management/components/FolderUploadTab.tsx
+++ web/app/routes/$userName+/page-management/components/FolderUploadTab.tsx
@@ -3,7 +3,6 @@ import { TranslationInputForm } from "./TranslationInputForm";
 export function FolderUploadTab() {
 	return (
 		<div className="space-y-6">
-			<div className="flex flex-col space-y-4"></div>
 			<TranslationInputForm />
 		</div>
 	);
